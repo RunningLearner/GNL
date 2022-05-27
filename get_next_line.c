f16294004fa9e4f9ba3cf15c95cfb80b6b2785c5
seunam <seunam@student.42seoul.kr>
2022-05-27 18:12:02 +0900

    CHORE: var naming

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: seunam <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 10:19:09 by seunam            #+#    #+#             */
-/*   Updated: 2022/05/27 18:03:10 by seunam           ###   ########.fr       */
+/*   Updated: 2022/05/27 18:09:50 by seunam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,18 @@ size_t	ft_strlen(const char *s)
 
 static char	*ft_read_line(int fd, char *buf, char *backup)
 {
-	int	count;
+	int	cnt;
 	char	*tmp;
 
-	count = 1;
-	while (count)
+	cnt = 1;
+	while (cnt)
 	{
-		count = read(fd, buf, BUFFER_SIZE);
-		if (count == -1)
+		cnt = read(fd, buf, BUFFER_SIZE);
+		if (cnt == -1)
 			return (0);
-		else if (count == 0)
+		else if (cnt == 0)
 			break ;
-		buf[count] = '\0';
+		buf[cnt] = '\0';
 		if (!backup)
 			backup = ft_strdup("");
 		tmp = backup;
@@ -50,27 +50,27 @@ static char	*ft_read_line(int fd, char *buf, char *backup)
 	return (backup);
 }
 
-static char	*ft_extract(char *line)
+static char	*ft_cut_line(char *line)
 {
 	int   i;
-	char  *result;
+	char  *res;
 
 	i = 0;
 	while (line[i] != '\n' && line[i] != '\0')
 		i++;
 	if (line[i] == '\0')
 		return (0);
-	result = ft_substr(line, i + 1, ft_strlen(line) - i);
-	if (!result)
+	res = ft_substr(line, i + 1, ft_strlen(line) - i);
+	if (!res)
 		return (NULL);
-	if (result[0] == '\0')
+	if (res[0] == '\0')
        {
-	       free(result);
-	       result = NULL;
+	       free(res);
+	       res = NULL;
 	       return (NULL);
        }
 	line[i + 1] = '\0';
-	return (result);
+	return (res);
 }
 
 char	*get_next_line(int fd)
@@ -89,6 +89,6 @@ char	*get_next_line(int fd)
 	buf = NULL;
 	if (!line)
 		return (NULL);
-	backup = ft_extract(line);
+	backup = ft_cut_line(line);
 	return (line);
 }
