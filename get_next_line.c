ca10cd2c509d867260ba698af50d4b7e37c1c863
seunam <seunam@student.42seoul.kr>
2022-05-27 18:03:50 +0900

    CHORE: norm applied

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: seunam <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 10:19:09 by seunam            #+#    #+#             */
-/*   Updated: 2022/05/27 17:26:07 by seunam           ###   ########.fr       */
+/*   Updated: 2022/05/27 18:03:10 by seunam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,73 +22,73 @@ size_t	ft_strlen(const char *s)
 	return (cnt);
 }
 
-static char *ft_read_line(int fd, char *buf, char *backup)
+static char	*ft_read_line(int fd, char *buf, char *backup)
 {
-  int   count;
-  char  *temp_pointer;
+	int	count;
+	char	*tmp;
 
-  count = 1;
-  while (count)
-  {
-    count = read(fd, buf, BUFFER_SIZE);
-    if (count == -1)
-      return (0);
-    else if (count == 0)
-      break ;
-    buf[count] = '\0';
-    if (!backup)
-      backup = ft_strdup("");
-    temp_pointer = backup;
-    backup = ft_strjoin(temp_pointer, buf);
-    if (!backup)
-      return (NULL);
-    free(temp_pointer);
-    temp_pointer = NULL;
-    if (ft_strchr(buf, '\n'))
-      break ;
-  }
-  return (backup);
+	count = 1;
+	while (count)
+	{
+		count = read(fd, buf, BUFFER_SIZE);
+		if (count == -1)
+			return (0);
+		else if (count == 0)
+			break ;
+		buf[count] = '\0';
+		if (!backup)
+			backup = ft_strdup("");
+		tmp = backup;
+		backup = ft_strjoin(tmp, buf);
+		if (!backup)
+			return (NULL);
+		free(tmp);
+		tmp = NULL;
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	return (backup);
 }
 
-static char *ft_extract(char *line)
+static char	*ft_extract(char *line)
 {
-  int   i;
-  char  *result;
+	int   i;
+	char  *result;
 
-  i = 0;
-  while (line[i] != '\n' && line[i] != '\0')
-    i++;
-  if (line[i] == '\0')
-    return (0);
-  result = ft_substr(line, i + 1, ft_strlen(line) - i);
-  if (!result)
-    return (NULL);
-  if (result[0] == '\0')
-  {
-    free(result);
-    result = NULL;
-    return (NULL);
-  }
-  line[i + 1] = '\0';
-  return (result);
+	i = 0;
+	while (line[i] != '\n' && line[i] != '\0')
+		i++;
+	if (line[i] == '\0')
+		return (0);
+	result = ft_substr(line, i + 1, ft_strlen(line) - i);
+	if (!result)
+		return (NULL);
+	if (result[0] == '\0')
+       {
+	       free(result);
+	       result = NULL;
+	       return (NULL);
+       }
+	line[i + 1] = '\0';
+	return (result);
 }
 
-char  *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-  char    *line;
-  char    *buf;
-  static char *backup;
+	char	*line;
+	char	*buf;
+	static char	*backup;
 
-  if (fd < 0 || BUFFER_SIZE <= 0)
-    return (NULL);
-  buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-  if (!buf)
-    return (NULL);
-  line = ft_read_line(fd, buf, backup);
-  free(buf);
-  buf = NULL;
-  if (!line)
-    return (NULL);
-  backup = ft_extract(line);
-  return (line);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	line = ft_read_line(fd, buf, backup);
+	free(buf);
+	buf = NULL;
+	if (!line)
+		return (NULL);
+	backup = ft_extract(line);
+	return (line);
 }
