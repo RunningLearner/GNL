75324d54a18bf7ec328b4f54a644c0c45cb93639
RunningLearner <97277365+RunningLearner@users.noreply.github.com>
2022-08-23 12:56:00 +0900

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -36,31 +36,31 @@ char	*ft_read(int fd, char *backup)
 	return (backup);
 }
 
-char	*ft_return_oneline(char *backup) // read 한 backup를 파라미터로 받음
+char	*ft_return_oneline(char *backup)
 {
 	int		i;
 	char	*str;
 
 	i = 0;
-	if (!backup[i]) // backup가 비어있음.
+	if (!backup[i])
 		return (NULL);
-	while (backup[i] && backup[i] != '\n') // 개행을 만나거나 backup 가 끝날 때까지
+	while (backup[i] && backup[i] != '\n')
 		i++;
-	str = (char *)malloc(sizeof(char) * (i + 2)); // 개행을 넣어줄 수도 있기 때문에 i + 2
+	str = (char *)malloc(sizeof(char) * (i + 2));
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (backup[i] && backup[i] != '\n') // 개행을 만나거나 backup가 끝날 때까지
+	while (backup[i] && backup[i] != '\n')
 	{
-		str[i] = backup[i]; // 복사해줌
+		str[i] = backup[i];
 		i++;
 	}
-	if (backup[i] == '\n') // 개행을 만난거라면
+	if (backup[i] == '\n')
 	{
-		str[i] = backup[i]; // 개행 넣어줌
+		str[i] = backup[i];
 		i++;
 	}
-	str[i] = '\0'; // 마지막 널값 넣어줌
+	str[i] = '\0';
 	return (str);
 }
 
@@ -72,19 +72,19 @@ char	*ft_backup(char *backup)
 
 	i = 0;
 	j = 0;
-	while (backup[i] && backup[i] != '\n') // 자른 곳까지 인덱스를 보내줌.
+	while (backup[i] && backup[i] != '\n')
 		i++;
-	if (!backup[i]) // 그 뒤에값이 더 이상 없다면 (끝까지 읽은 경우)
+	if (!backup[i])
 	{
 		free(backup);
 		return (NULL);
 	}
-	tmp = (char *)malloc(sizeof(char) * (ft_strlen(backup) - i)); // 자르고 담아놓을 문자열 (backup 총 길이 - 이미 잘라서 보내 놓은 것들 + 1(널값))
+	tmp = (char *)malloc(sizeof(char) * (ft_strlen(backup) - i));
 	if (!tmp)
 		return (NULL);
-	i++; // 개행 다음부터 시작하기 위해
+	i++;
 	while (backup[i])
-		tmp[j++] = backup[i++]; // 복사해줌
+		tmp[j++] = backup[i++];
 	tmp[j] = '\0';
 	free(backup);
 	return (tmp);
@@ -97,10 +97,10 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	backup = ft_read(fd, backup); // 읽어와서 backup 에 저장시켜둠
+	backup = ft_read(fd, backup);
 	if (!backup)
-		return (NULL); // backup 에 제대로 못 받아왔을 경우 (read 리턴값이 -1 일 때)
-	realline = ft_return_oneline(backup); // backup를 개행 이나 널값 전까지 복사해준 문자열을 리턴
-	backup = ft_backup(backup); // 남은거 backup에 저장시켜둔 채로 종료 static char 형이기 때문에 다음 번에 불러와도 이 상태의 backup를 기억하고 있음.
+		return (NULL);
+	realline = ft_return_oneline(backup);
+	backup = ft_backup(backup);
 	return (realline);
 }
