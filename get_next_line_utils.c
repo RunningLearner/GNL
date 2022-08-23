e31364632f9c8b7e7f6d32ad8dfac85ec72274dd
RunningLearner <97277365+RunningLearner@users.noreply.github.com>
2022-08-23 11:09:51 +0900

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -12,101 +12,60 @@
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s)
+size_t	ft_strlen(char *str)
 {
-	size_t	cnt;
+	size_t	c;
 
-	cnt = 0;
-	while (*s ++)
-		cnt ++;
-	return (cnt);
+	c = 0;
+	if (!str)
+		return (0);
+	while (str[c] != '\0')
+		c++;
+	return (c);
 }
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strchr(char *str, char c)
 {
-	char			*ptr;
-	unsigned int	i;
-	unsigned int	j;
+	int	i;
 
-	if (len > ft_strlen(s))
+	i = 0;
+	if (!str)
+		return (0);
+	if (c == '\0')
+		return ((char *)&str[ft_strlen(str)]);
+	while (str[i] != '\0')
 	{
-		len = ft_strlen(s);
-	}
-	ptr = (char *)malloc(len + 1);
-	i = start;
-	j = 0;
-	if (ptr == NULL)
-	{
-		return (NULL);
-	}
-	while (j < len && i < ft_strlen(s))
-	{
-		ptr[j] = s[i];
+		if (str[i] == c)
+			return ((char *)&str[i]);
 		i++;
-		j++;
 	}
-	ptr[j] = '\0';
-	return (ptr);
+	return (0);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
+	size_t	i;
+	size_t	j;
 	char	*str;
-	size_t	strlen;
-	size_t	idx;
 
+	if (!s1)
+	{
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
 	if (!s1 || !s2)
 		return (NULL);
-	strlen = ft_strlen((char *)s1) + ft_strlen((char *)s2);
-	str = malloc(sizeof(char) * (strlen + 1));
-	if (!str)
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (str == NULL)
 		return (NULL);
-	idx = 0;
-	while (*s1)
-	{
-		str[idx++] = *s1++;
-	}
-	while (*s2)
-	{
-		str[idx++] = *s2++;
-	}
-	str[idx] = 0;
+	i = -1;
+	j = 0;
+	if (s1)
+		while (s1[++i] != '\0')
+			str[i] = s1[i];
+	while (s2[j] != '\0')
+		str[i++] = s2[j++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
 	return (str);
-}
-
-char	*ft_strdup(const char *s)
-{
-	char	*dup;
-	size_t	len;
-	size_t	idx;
-	char	*ss;
-
-	ss = (char *)s;
-	len = 0;
-	while (s[len])
-		len ++;
-	dup = malloc(sizeof(char) * (len + 1));
-	if (!dup)
-		return (NULL);
-	idx = 0;
-	while (ss[idx])
-	{
-		dup[idx] = ss[idx];
-		idx ++;
-	}
-	dup[idx] = 0;
-	return (dup);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s)
-	{
-		if ((unsigned char)*s == (unsigned char)c)
-			return ((char *)s);
-		s ++;
-	}
-	if ((unsigned char)*s == (unsigned char)c)
-		return ((char *)s);
-	return (0);
 }
