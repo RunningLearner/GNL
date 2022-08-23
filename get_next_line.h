2527accc17b32e1421974c8d46b8cf080883dcb8
RunningLearner <97277365+RunningLearner@users.noreply.github.com>
2022-08-23 13:29:28 +0900

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -22,6 +22,6 @@ char		*ft_strjoin(char *s1, char*s2);
 char		*get_next_line(int fd);
 char		*ft_read(int fd, char *word);
 char		*ft_return_oneline(char *word);
-char		*ft_backup(char *word);
+char		*ft_cut_backup(char *word);
 
 #endif
